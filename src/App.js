e5fb7747c38917905e07ae129770d2c3eb10e9d9
annusingh2403@gmail.com <annusingh2403@gmail.com>
2021-09-04 14:56:08 +0530

    i commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+// import Signup from './components/SignUp';
+// import Login from './components/Login';
+import ErrorPage from './components/ErrorPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/> 
+
+    <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/contact">
+        <Contact/>
+      </Route>
+      {/* <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/signup">
+        <Signup/>
+      </Route> */}
+
+      <Route>
+        <ErrorPage/>
+      </Route>
+    </Switch>
+  </>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Comming Soon 😃</h1>
+            <p>Work In Progress.....</p>
+        </div>
+    )
+}
+
+export default Home;
